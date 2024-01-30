a1b14691c050bbded06141796a62a4a9f292a9e2
unknown <BT_4N2_06@BKTECH.LOCAL>
2024-01-30 14:02:52 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Cat> cats = new ArrayList<Cat>();
+        ArrayList<Dog> dogs = new ArrayList<Dog>();
+        cats.add(new Cat("tails"));
+        
+
+
+    }
+}
